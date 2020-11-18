7dd65663a6b37fe3dd28318e7476a9f8560c4ae8
Sumaiya Tasnim <tasnim@umich.edu>
2020-11-17 22:12:07 -0500

    Testing AddBcc()

---
+++ commons-email/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,50 @@
+package org.apache.commons.mail;
+import org.junit.Before;
+import org.junit.Test;
+
+//import java.sql.SQLException;
+//import java.util.Date;
+
+//import javax.mail.Address;
+//import javax.mail.internet.MimeMessage;
+//import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+
+
+public class EmailTest {
+	public static final String[] TEST_EMAILS = {"ab@bc.com", "a.b@c.org", "abgfrtbv@srvvb.com.bd" };
+	public static final String TEST_EM = "ab@bc.com";
+	public static final String TEST_EM1 = null;
+	public static final String TEST_VNAME = "Sumaiya";
+	public static final String TEST_VNAME1 = null;
+
+	
+	
+	
+	/* Concrete Email Class for testing */
+	private EmailConcrete email;
+	//private EmailConcrete em;
+	@Before
+	public void setUpEmailTest() throws Exception{
+		
+		email = new EmailConcrete();
+		
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception{
+		
+	}
+	
+	/* 
+	 * Test addBcc(String email) function
+	 */
+	@Test
+	public void testAddBcc() throws Exception {
+		
+		email.addBcc(TEST_EMAILS);
+		assertEquals(3, email.getBccAddresses().size());
+	}
+}
